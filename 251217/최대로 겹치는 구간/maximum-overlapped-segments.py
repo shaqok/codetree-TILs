525ca092791b0e1677fad3fbcef4aa0fa75f2925
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-17 22:39:22 +0900

    Title: 최대로 겹치는 구간, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251217/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 251217/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -4,7 +4,7 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 arr = [0] * 200
 
-
+# 겹치는 구간을 찾으려면 start를 기준으로 기록, end - 1까지
 for segment in segments:
     start, end = segment[0] + 100, segment[1] + 100
     for i in range(start, end):
