4b2b52311d54ee5ec0f56e28662d03ca59b8eb19
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-17 22:31:45 +0900

    Title: 블럭쌓는 명령2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251217/블럭쌓는 명령2/block-stacking-commands2.py
+++ 251217/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,11 +2,10 @@ n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
-grid = [0] * n
+grid = [0] * (n+1)
 
 for command in commands:
     a, b = command
     for i in range(a, b+1):
         grid[i] += 1
-
 print(max(grid))
