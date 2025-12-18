0687761519c6d38971e5928557d56cebfe7fb435
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-19 00:47:27 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 87ms, Memory: 48MB, Status: Wrong Answer - Codetree

--- 251219/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 251219/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -13,8 +13,8 @@ for k in range(2):
     cur_x1, cur_x2 = x1[k] + 1000, x2[k] + 1000
     cur_y1, cur_y2 = y1[k] + 1000, y2[k] + 1000
 
-    for i in range(cur_x1, cur_x2):
-        for j in range(cur_y1, cur_y2):
+    for i in range(cur_x1, cur_x2 + 1):
+        for j in range(cur_y1, cur_y2 + 1):
             grid[i][j] = k + 1
 
 
@@ -30,11 +30,11 @@ max_height = cur_x1
 
 max_width_len = 0
 
-for i in range(cur_x1, cur_x2):
+for i in range(cur_x1, cur_x2 + 1):
     width_count = 0
     cur_min_width = cur_y1
     cur_max_width = cur_y1
-    for j in range(cur_y1, cur_y2):
+    for j in range(cur_y1, cur_y2 + 1):
         if width_count == 0 and grid[i][j] == 1:
             cur_min_width = j
             cur_max_width = j
@@ -45,9 +45,9 @@ for i in range(cur_x1, cur_x2):
     if 1 in set(grid[i]):
         max_height = i
     
-    max_width_len = max(max_width_len, (cur_max_width - cur_min_width + 1))
+    max_width_len = max(max_width_len, (cur_max_width - cur_min_width))
 
-print(max_width_len * (max_height - min_height + 1))
+print(max_width_len * (max_height - min_height))
 
 '''
 5 5 15 15
