09fc4d77171c79fd20a9165c7a2d3880cacbb2fa
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-19 00:50:40 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 134ms, Memory: 48MB, Status: Passed - Codetree

--- 251219/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 251219/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,55 +1,49 @@
-x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
-x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
-x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
+OFFSET = 1000
+MAX_R = 2000
 
-# Please write your code here.
-MAX_LEN = 2001
+# 변수 선언 및 입력
+n = 2
+rects = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-grid = []
-for _ in range(MAX_LEN):
-    grid.append([0] * MAX_LEN)
+checked = [
+    [0] * (MAX_R + 1)
+    for _ in range(MAX_R + 1)
+]
 
-for k in range(2):
-    cur_x1, cur_x2 = x1[k] + 1000, x2[k] + 1000
-    cur_y1, cur_y2 = y1[k] + 1000, y2[k] + 1000
-
-    for i in range(cur_x1, cur_x2 + 1):
-        for j in range(cur_y1, cur_y2 + 1):
-            grid[i][j] = k + 1
-
-
-cur_x1, cur_x2 = x1[0] + 1000, x2[0] + 1000
-cur_y1, cur_y2 = y1[0] + 1000, y2[0] + 1000
-
-# for i in range(x1[1] + 1000, x2[1] + 1000):
-#     # for j in range(cur_y1, cur_y2):
-#     print(grid[i][cur_y1:cur_y2])
-
-min_height = cur_x1
-max_height = cur_x1
-
-max_width_len = 0
-
-for i in range(cur_x1, cur_x2 + 1):
-    width_count = 0
-    cur_min_width = cur_y1
-    cur_max_width = cur_y1
-    for j in range(cur_y1, cur_y2 + 1):
-        if width_count == 0 and grid[i][j] == 1:
-            cur_min_width = j
-            cur_max_width = j
-            width_count += 1
-        elif grid[i][j] == 1:
-            cur_max_width = j
-            width_count += 1
-    if 1 in set(grid[i]):
-        max_height = i
+for i, (x1, y1, x2, y2) in enumerate(rects, start=1):
+    # OFFSET을 더해줍니다.
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
     
-    max_width_len = max(max_width_len, (cur_max_width - cur_min_width))
+    # 직사각형에 주어진 순으로 1, 2 번호를 붙여줍니다.
+    # 격자 단위로 진행하는 문제이므로
+    # x2, y2에 등호가 들어가지 않음에 유의합니다.
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            checked[x][y] = i
 
-print(max_width_len * (max_height - min_height))
+# 1, 2 순으로 붙였는데도
+# 아직 숫자 1로 남아있는 곳들 중 최대 최소 x, y 값을 전부 계산합니다.
+min_x, max_x, min_y, max_y = MAX_R, 0, MAX_R, 0
+first_rect_exist = False
+for x in range(MAX_R + 1):
+    for y in range(MAX_R + 1):
+        if checked[x][y] == 1:
+            first_rect_exist = True
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
 
-'''
-5 5 15 15
-3 9 18 12
-'''
+# 넓이를 계산합니다.
+# Case 1. 첫 번째 직사각형이 전혀 남아있지 않다면 넓이는 0입니다.
+if not first_rect_exist:
+    area = 0
+# Case 2. 첫 번째 직사각형이 남아있다면, 넓이를 계산합니다.
+else:
+    area = (max_x - min_x + 1) * (max_y - min_y + 1)
+
+print(area)
