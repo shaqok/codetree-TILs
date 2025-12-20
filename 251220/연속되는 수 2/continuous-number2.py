7e9c42c46f68e0a1fdcf2de88f0bdfe0a5a4b0e8
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-20 13:37:26 +0900

    Title: 연속되는 수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251220/연속되는 수 2/continuous-number2.py
+++ 251220/연속되는 수 2/continuous-number2.py
@@ -7,15 +7,15 @@ arr = [int(input()) for _ in range(n)]
 
 if n == 1:
     print(1)
+else:
+    max_freq = 0
 
-max_freq = 0
+    prev_i = 0
+    for i in range(n):
+        if i == 0 or arr[i] != arr[i-1]:
+            freq = i - prev_i
+            max_freq = max(max_freq, freq)
+            prev_i = i
 
-prev_i = 0
-for i in range(n):
-    if i == 0 or arr[i] != arr[i-1]:
-        freq = i - prev_i
-        max_freq = max(max_freq, freq)
-        prev_i = i
-
-print(max_freq)
+    print(max_freq)
     
