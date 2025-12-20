2dc8b1f0cebd5ba541feab62c742a7d91074152b
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2025-12-20 16:51:51 +0900

    Title: 좌우로 움직이는 로봇, Time: 126ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 251220/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 251220/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -51,15 +51,11 @@ while t or d or t_b or d_b:
             t_b.pop()
             d_b.pop()
     
-    if cur_pos_a == cur_pos_b:
-        if not prev_met:
-            met_cnt += 1
-            prev_met = True
+    if cur_pos_a == cur_pos_b and not prev_met:
+        met_cnt += 1
+        prev_met = True
     elif cur_pos_a != cur_pos_b and prev_met:
         prev_met = False
-
-    # timestamp += 1
-    # print(cur_pos_a, cur_pos_b, timestamp)
     
 
 print(met_cnt)
