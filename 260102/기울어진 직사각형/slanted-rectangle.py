6d61ac09aecbc4d4a90c22d79ffb6af0aeef7297
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-02 15:53:00 +0900

    Title: 기울어진 직사각형, Time: 134ms, Memory: 21MB, Status: Passed - Codetree

--- 260102/기울어진 직사각형/slanted-rectangle.py
+++ 260102/기울어진 직사각형/slanted-rectangle.py
@@ -1,74 +1,32 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-'''
-L자로 순회하도록
-r = 2
-c = 1
-'''
-r, c = 2, 1
-max_sum = 0
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def get_rectangle_sum(r, c):
-    '''
-    1. r-1, c+1 until r-2 >= 0 and c+1 < n
-    2. r-1, c-1
-    3. r+1, c-1 until r+2 < n and c-1 >= 0
-    4. r+1, c+1
-    '''
+def get_score(x, y, k, l):
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+    move_nums = [k, l, k, l]
+
     cur_sum = 0
-    cur_r, cur_c = r, c
-    while True:
-        cur_sum += grid[cur_r][cur_c]
-        if cur_r - 1 == 0 or cur_c + 1 == n:
-            break
-        cur_r -= 1
-        cur_c += 1
-    
-    while True:
-        cur_r -= 1
-        cur_c -= 1
-        if cur_r >= 0:
-            cur_sum += grid[cur_r][cur_c]
-            if cur_r == 0:
-                break
-    
-    while True:
-        cur_r += 1
-        cur_c -= 1
-        if cur_r < n - 1 and cur_c >= 0:
-            cur_sum += grid[cur_r][cur_c]
-            if cur_r == n - 2 or cur_c == 0:
-                break
-    
-    while True:
-        cur_r += 1
-        cur_c += 1
-        if cur_r < n and cur_c < n:
-            if cur_r == r and cur_c == c:
-                break
-            cur_sum += grid[cur_r][cur_c]
+
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x, y = x + dx, y + dy
+
+            if not in_range(x, y):
+                return 0
+            
+            cur_sum += grid[x][y]
     
     return cur_sum
 
+max_sum = 0
 
-# for i in range(2, n):
-#     for j in range(1, n-1):
-#         max_sum = max(max_sum, get_rectangle_sum(i, j))
-    
-# print(max_sum)
-
-
-# 아래로 순회
-while r < n:
-    max_sum = max(max_sum, get_rectangle_sum(r, c))
-    r += 1
-
-r = n - 1
-
-while c < n - 1:
-    max_sum = max(max_sum, get_rectangle_sum(r, c))
-    c += 1
+for i in range(n):
+    for j in range(n):
+        for k in range(1, n):
+            for l in range(1, n):
+                max_sum = max(max_sum, get_score(i, j, k, l))
 
 print(max_sum)
-
