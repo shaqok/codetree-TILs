0754bd6e4490a8675f8b521df3bf5ecbbadccc3c
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-20 20:14:48 +0900

    Title: 단어 정렬, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260120/단어 정렬/sorting-words.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const words = input.slice(1, n + 1);
+// Please Write your code here.
+
+words.sort()
+for (let word of words) {
+    console.log(word);
+}
