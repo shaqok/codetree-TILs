1a76647f51a476761cef189ebc0f6642b6372f97
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-20 18:17:48 +0900

    Title: 반복 출력하기, Time: 69ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260120/반복 출력하기/repeated-output.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+function func(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("12345^&*()_");
+    }
+}
+
+func(n)
+
