7d9b63053279d6e844a0ef05f0e8574f865473d3
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-20 18:52:56 +0900

    Title: 반복 출력하기 2, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260120/반복 출력하기 2/repeated-output-2.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+
+// Please Write your code here.
+function recur(n) {
+    if (n == 0) {
+        return
+    }
+    console.log("HelloWorld");
+    recur(n-1);
+}
+
+recur(4);
