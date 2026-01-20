5790cdf7c08b195271af9471a695c83d7c243dcf
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-20 21:03:33 +0900

    Title: 체크판위에서 2, Time: 63ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260120/체크판위에서 2/on-the-checkboard-2.js
+++ 260120/체크판위에서 2/on-the-checkboard-2.js
@@ -6,19 +6,15 @@ const grid = input.slice(1).map(line => line.trim().split(' '));
 
 // Please Write your code here.
 let total_cnt = 0;
-for (let i = 0; i < R-2; i++) {
-    for (let j = 0; j < C-2; j++) {
-        for (let k = i + 1; k < R-1; k++) {
-            for (let l = j + 1; l < C-1; l++) {
-                for (let n = k + 1; n < R; n++) {
-                    for (let m = l + 1; m < C; m++) {
-                        let start = grid[i][j];
-                        let firstMove = grid[k][l];
-                        let secondMove = grid[n][m];
-                        if (start !== firstMove && firstMove !== secondMove && (n !== R-1 && m !== C-1)) {
-                            total_cnt++;
-                        }
-                    }
+
+for (let k = 1; k < R-1; k++) {
+    for (let l = 1; l < C-1; l++) {
+        for (let n = k + 1; n < R; n++) {
+            for (let m = l + 1; m < C; m++) {
+                let firstMove = grid[k][l];
+                let secondMove = grid[n][m];
+                if (grid[0][0] !== firstMove && firstMove !== secondMove && (n !== R-1 && m !== C-1)) {
+                    total_cnt++;
                 }
             }
         }
