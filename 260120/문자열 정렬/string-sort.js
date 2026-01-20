cb3af93a43550a723e9a52e283b76d47103458e8
Daniel Daeyeon Kim <54762756+shaqok@users.noreply.github.com>
2026-01-20 20:13:40 +0900

    Title: 문자열 정렬, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260120/문자열 정렬/string-sort.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const s = input[0];
+// Please Write your code here. 
+
+let arr = [...s];
+arr.sort();
+console.log(arr.join(""))
